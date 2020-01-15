19cd3c6587aab5218f43e6d5ddad576581d4c550
wpramio <willianpramio@hotmail.com>
2020-01-15 10:54:49 -0300

    updated messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+    def new
+        @message = Message.new
+    end
+
+    def create
+        @message = Message.create(msg_params)
+        if @message.save
+            ActionCable.server.broadcast 'room_channel',
+            timestamp: @message.created_at.advance(hours: -3).to_formatted_s(:db),
+            content: @message.content
+        end
+    end
+
+    private
+        def msg_params
+            params.require(:message).permit(:content)
+        end
 end
