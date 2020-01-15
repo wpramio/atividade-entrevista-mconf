b06ff3c84d376407dddd56c0fb692b2adc383dab
wpramio <willianpramio@hotmail.com>
2020-01-15 12:46:06 -0300

    updated Message model (contains_mconf)

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,10 @@
 class Message < ApplicationRecord
     validates :content, presence: true
+    @contains_mconf = false
+    def contains_mconf?
+        return @contains_mconf
+    end
+    def contains_mconf!
+        @contains_mconf = true
+    end
 end
