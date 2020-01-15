62df2977fec438e90ba16e86ddd97d14242c3e0b
wpramio <willianpramio@hotmail.com>
2020-01-15 12:38:20 -0300

    updated Message controller (lookfor_mconf)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,7 @@ class MessagesController < ApplicationController
 
     def create
         @message = Message.create(msg_params)
+        @message.content = lookfor_mconf(@message.content)
         if @message.save
             ActionCable.server.broadcast 'room_channel',
             timestamp: @message.created_at.advance(hours: -3).to_formatted_s(:db),
@@ -15,5 +16,8 @@ class MessagesController < ApplicationController
     private
         def msg_params
             params.require(:message).permit(:content)
+        end
+        def lookfor_mconf(str)
+            return str.gsub(/mconf/i, 'Mconf')
         end
 end
